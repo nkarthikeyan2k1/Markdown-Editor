210d6767a3eb9292cd98ab0a395d9be6a83524d2
nkarthikeyan2k1 <nkarthikeyan2k1@gmail.com>
2024-05-05 17:44:02 +0530

    intial commit

---
+++ api/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const path = require("path");
+require("dotenv").config({ path: ".env" });
+const cors = require("cors");
+app.use(cors({}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const PORT = process.env.PORT || 3001;
+app.get("/", (req, res) => {
+  return res.status(200).json({ message: `server connected  port ${PORT}` });
+});
+
+server.listen(PORT, () => {
+  console.log(`server started at  ${PORT}`);
+});
